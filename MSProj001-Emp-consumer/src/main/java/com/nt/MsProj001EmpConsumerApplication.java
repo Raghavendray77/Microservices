21445554a26951522cff4ace062b93a82270c7f3
Raghaven <Raghaven@gmail.com>
2024-03-14 11:55:48 +0530

    first commit

---
+++ MSProj001-Emp-consumer/src/main/java/com/nt/MsProj001EmpConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsProj001EmpConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsProj001EmpConsumerApplication.class, args);
+	}
+
+}
